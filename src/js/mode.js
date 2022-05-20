2bfee867665aa1b23a23dd373ddbb55305847d3e
IgorTLA <igort.lira@gmail.com>
2022-05-20 18:25:22 -0300

    Some changes

--- src/js/mode.js
+++ src/js/mode.js
@@ -10,3 +10,17 @@ modeToggle.addEventListener('click', () => {
 sidebarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('close')
 })
+
+//Adicionando Active class na tab selecionada
+// const list = document.querySelectorAll('li')
+
+// function activeLink() {
+//     list.forEach((item) =>
+//         item.classList.remove('active'))
+//     this.classList.add('active')
+// }
+
+// console.log(activeLink())
+
+// list.forEach((item) =>
+//     item.addEventListener('click', activeLink))
